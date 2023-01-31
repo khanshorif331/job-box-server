ba1ee187b7178d5ab2e519133868e7d2d8fde9c9
khanshorif331 <khanshorif331@gmail.com>
2023-01-31 21:52:14 +0600

    cors errors solved

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
+	next()
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jtdcwel.mongodb.net/?retryWrites=true&w=majority`
